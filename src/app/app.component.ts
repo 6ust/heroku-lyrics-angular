99394a3f860c4af5176c364efd440c57f24701aa
Gust <gust.guedes95@gmail.com>
2021-03-30 11:38:44 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lyrics-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lyrics-redirect-angular';
+}
